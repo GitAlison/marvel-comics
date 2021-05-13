c1438736a23e1fde8fb31501a376f2f3d20a6c21
Alison Aguiar <alysson.k888@gmail.com>
2021-05-13 10:24:45 -0300

    Variaveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MARVEL_PUBLIC_KEY: 'f3772d7a86a20c6dd32a00084b24f860',
+  MARVEL_HASH: 'e1fe92f50d1156b228a9231264c5a7f5',
+  BASE_API: 'http://gateway.marvel.com/v1/public/'
 };
 
 /*
